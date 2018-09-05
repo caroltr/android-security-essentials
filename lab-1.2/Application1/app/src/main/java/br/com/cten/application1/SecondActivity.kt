3e763aaa182c7602d28f3b65c8d4d6a17a8dd65d
Caroline Tenorio Ribeiro <caroltr.srs@gmail.com>
2018-09-04 23:17:22 -0300

    Add application 1 of lab 1.2

---
+++ lab-1.2/Application1/app/src/main/java/br/com/cten/application1/SecondActivity.kt
@@ -0,0 +1,15 @@
+package br.com.cten.application1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by caroltr on 04/09/2018.
+ */
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
